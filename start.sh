e4a109cd0210b8e45508b4a454ef9be204b84a3d
飞走的纸飞机 <1749748955@qq.com>
2024-09-01 09:56:35 +0000

    add start file

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+docker run --rm -it  \
+    -p 8888:8888 \
+    -v $PWD:$PWD \
+    -w $PWD \
+    --user  33333  --group-add users \
+    -e DOCKER_STACKS_JUPYTER_CMD="notebook" \
+    -e NOTEBOOK_ARGS="--NotebookApp.token='' " \
+    registry.cn-hangzhou.aliyuncs.com/wybioinfo/keras-notebook:3.5.0
